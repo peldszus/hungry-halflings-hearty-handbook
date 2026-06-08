46697632de7eda160a1194b9bdb63709b9890b57
peldszus <peldszus@users.noreply.github.com>
2026-06-08 15:47:59 +0000

    deploy: 1aa0abdfddeaa29b58edd41d7eaab985073e7734

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hungry-halflings-hearty-handbook/sw.js', { scope: '/hungry-halflings-hearty-handbook/' })})}
